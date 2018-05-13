282bf1889090d20cb172842a6063ce2ebaf08b08
Wahyu Kristanto <wahyu.kristanto@ti.ukdw.ac.id>
2018-05-13 20:39:46 +0700

    poin nomor 2 belum berhasil

---
+++ Dadu.java
@@ -0,0 +1,92 @@
+import java.util.Random;
+import java.awt.Color;
+
+public final class Dadu {
+  public static final int DEFAULT_SISI = 6;
+
+  private static Random randGenerator = new Random();
+
+  private final int jumSisi;
+  private String[] warnaSisi;
+  private int hasil;
+  private int[] cekBedaNilai;
+  private int perulangan;
+
+  public Dadu() {
+    this(DEFAULT_SISI);
+  }
+
+  public Dadu(int numSides) {
+    this.jumSisi = numSides;
+    int perulangan = 0;
+    cekBedaNilai = new int[jumSisi];
+    randomWarna();
+  }
+
+  public void randomWarna(){
+    int random;
+    warnaSisi = new String[jumSisi];
+
+    for(int i=0; i<jumSisi; i++){
+      random = randGenerator.nextInt(16777215);
+      Color sisi = new Color(random);
+
+      if(i != 0){
+        for(int j=0; j<i; j++){
+          while(sisi.toString() == warnaSisi[j]){
+            random = randGenerator.nextInt(16777215);
+            sisi = new Color(random);
+          }
+        }
+      }
+
+      warnaSisi[i] = sisi.toString();
+    }
+
+    for(int i=0; i<jumSisi; i++){
+      System.out.println(i +" = " +warnaSisi[i]);
+    }
+  }
+
+  public String getWarnaSisi(int hasil){
+    return warnaSisi[hasil];
+  }
+
+  public int lempar() {
+    if(perulangan == jumSisi){
+      System.out.println("Semua sisi sudah ditampilkan");
+    }else{
+      hasil = randGenerator.nextInt(jumSisi) + 1;
+
+      if(perulangan != 0){
+        for(int j=0; j<perulangan; j++){
+          while(hasil == cekBedaNilai[j]){
+            hasil = randGenerator.nextInt(jumSisi) + 1;
+          }
+        }
+      }
+
+      cekBedaNilai[perulangan] = hasil;
+      System.out.println("Warna sisi ke-" +hasil +" adalah " +getWarnaSisi(hasil-1));
+      perulangan++;
+    }
+
+    return hasil;
+  }
+
+  public int getJumSisi() {
+    return jumSisi;
+  }
+
+  public int getHasil() {
+    return hasil;
+  }
+
+  @Override
+  public String toString() {
+    for(int i=0; i<jumSisi; i++){
+      System.out.println(i +" = " +cekBedaNilai[i]);
+    }
+    return "Jumlah sisi dadu: " + getJumSisi() + " hasil: " + getHasil();
+  }
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+  public static void main(String[] args) {
+    Dadu cobadadu = new Dadu();
+    for(int i=0; i<7; i++){
+      cobadadu.lempar();
+    }
+    System.out.println(cobadadu.toString());
+  }
+}
